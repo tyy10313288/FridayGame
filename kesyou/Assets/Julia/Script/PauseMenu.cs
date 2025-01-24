4d18774405a8ed271f845788848f1b20680a77de
tyy10313288 <tyy10313288@gmail.com>
2025-01-24 11:07:08 +0900

    pause menu

---
+++ kesyou/Assets/Julia/Script/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public GameObject SettingPanel;
+    
+    public Button SettingButton;
+    public Button ResumeButton;
+    public Button TitleButton;
+    public Button QuitButton;    
+
+    
+    public static bool GameIsPaused = false;
+
+    void Start()
+    {        
+        SettingPanel.SetActive(false);        
+    }
+    void Update()
+    {
+        SettingButton.onClick.AddListener(Pause);
+    }  
+
+    public void Pause()
+    {
+        SettingPanel.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void Resume()
+    {
+        SettingPanel.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+   
+}
