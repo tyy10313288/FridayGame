45b96ea6d03e9fd6c80493128c50c5533c0a2dd9
ikedaharuki2 <game22403007@gmail.com>
2025-01-27 12:19:57 +0900

    a

--- kesyou/Assets/C#/Branch.cs
+++ kesyou/Assets/C#/Branch.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using static Unity.Collections.AllocatorManager;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class Branch : MonoBehaviour
 {
@@ -18,18 +19,17 @@ public class Branch : MonoBehaviour
         _event = PlayerPrefs.GetInt("EVENT", 0);
         PlayerPrefs.SetInt("RESULT", 0);
 
-        if (_event == 1)
-        {
-            Invoke("Eyeshadow", 0);
-        }
-        else if(_event == 2)
-        {
-            Invoke("Eyebrows", 0);
-        }
-        else
-        {
+        if(_event == 0)
             Invoke("_Start", 0);
-        }
+
+        if (_event == 1)
+            Invoke("Eyeshadow", 0);
+
+        if(_event == 2)
+            Invoke("Eyebrows", 0);
+
+        if (_event == 3)
+            Invoke("Lip", 0);
     }
     public void branch()
     {
@@ -51,8 +51,9 @@ public class Branch : MonoBehaviour
                 Invoke("Black1", delaytime);
                 break;  
             case 5:
-                PlayerPrefs.SetInt("EVENT",1);  
-                SceneManager.LoadScene("EyeShadow");
+                PlayerPrefs.SetInt("EVENT",1);
+                Invoke("Event", 0);
+                Invoke("EventLoad", 3);
                 break;
             case 6:
                 Invoke("Black2", delaytime);
@@ -61,10 +62,12 @@ public class Branch : MonoBehaviour
                 Invoke("Red", delaytime);
                 break;
             case 8:
-                Invoke("Ear", delaytime);
+                _result = 9;
+                Invoke("Result", delaytime);
                 break;
             case 9:
-                Invoke("Tactilesense", delaytime);
+                _result = 10;
+                Invoke("Result", delaytime);
                 break;
             case 10:
                 break;
@@ -75,7 +78,9 @@ public class Branch : MonoBehaviour
                 Invoke("Purple1", delaytime);
                 break;
             case 13:
-                Invoke("Lip", delaytime);
+                PlayerPrefs.SetInt("EVENT", 2);
+                Invoke("Event", 0);
+                Invoke("EventLoad", 2.5f);
                 break;
             case 14:
                 break;
@@ -84,7 +89,8 @@ public class Branch : MonoBehaviour
                 break;
             case 16:
                 PlayerPrefs.SetInt("EVENT", 2);
-                SceneManager.LoadScene("EyebrowEvent");
+                Invoke("Event", 0);
+                Invoke("EventLoad", 2.5f);
                 break;
             case 18:
                 Invoke("Purple2", delaytime);
@@ -105,11 +111,12 @@ public class Branch : MonoBehaviour
                 Invoke("Result", delaytime);
                 break;
         }
+        PlayerPrefs.Save();
     }
     public void _Start()
     {
         GameObject fand = (GameObject)Resources.Load("ファンデーション");
-        Instantiate(fand, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(fand, new Vector2(5, 0), Quaternion.identity);
     }
 
     public void destroy()
@@ -124,98 +131,86 @@ public class Branch : MonoBehaviour
         GameObject green = (GameObject)Resources.Load("緑");
         GameObject black = (GameObject)Resources.Load("黒1");
 
-        Instantiate(usuallay, new Vector2(-3, 3), Quaternion.identity);
-        Instantiate(white, new Vector2(-3, -3), Quaternion.identity);
-        Instantiate(green, new Vector2(3, 3), Quaternion.identity);
-        Instantiate(black, new Vector2(3, -3), Quaternion.identity);
+        Instantiate(usuallay, new Vector2(-5, 3), Quaternion.identity);
+        Instantiate(white, new Vector2(-5, -3), Quaternion.identity);
+        Instantiate(green, new Vector2(5, 3), Quaternion.identity);
+        Instantiate(black, new Vector2(5, -3), Quaternion.identity);
     }
     public void Usually()
     {
         GameObject eyeshadow = (GameObject)Resources.Load("アイシャドウ");
 
-        Instantiate(eyeshadow, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(eyeshadow, new Vector2(5, 0), Quaternion.identity);
     }
     public void White()
     {
         GameObject black1 = (GameObject)Resources.Load("黒2");
         GameObject red = (GameObject)Resources.Load("赤");
 
-        Instantiate(black1, new Vector2(-3, 0), Quaternion.identity);
-        Instantiate(red, new Vector2(3, 0), Quaternion.identity);
+        Instantiate(black1, new Vector2(-5, 0), Quaternion.identity);
+        Instantiate(red, new Vector2(5, 0), Quaternion.identity);
     }
     public void Green()
     {
         GameObject ear = (GameObject)Resources.Load("耳");
         GameObject tactilesense = (GameObject)Resources.Load("触覚");
 
-        Instantiate(ear, new Vector2(3, 0), Quaternion.identity);
-        Instantiate(tactilesense, new Vector2(-3, 0), Quaternion.identity);
+        Instantiate(ear, new Vector2(5, 0), Quaternion.identity);
+        Instantiate(tactilesense, new Vector2(-5, 0), Quaternion.identity);
     }
     public void Black1()
     {
         GameObject eyelashes = (GameObject)Resources.Load("まつ毛");
 
-        Instantiate(eyelashes, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(eyelashes, new Vector2(5, 0), Quaternion.identity);
     }
     public void Black2()
     {
         GameObject lip = (GameObject)Resources.Load("リップ");
 
-        Instantiate(lip, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(lip, new Vector2(5, 0), Quaternion.identity);
     }
     public void Eyeshadow()
     {
         GameObject pink = (GameObject)Resources.Load("ピンク");
         GameObject purple1 = (GameObject)Resources.Load("紫1");
 
-        Instantiate(pink, new Vector2(-3, 0), Quaternion.identity);
-        Instantiate(purple1, new Vector2(3, 0), Quaternion.identity);
-    }
-    public void Tactilesense() 
-    {
-        GameObject piccolo = (GameObject)Resources.Load("ピッコロ");
-
-        Instantiate(piccolo, new Vector2(0, 0), Quaternion.identity);
-    }
-    public void Ear()
-    {
-        GameObject shrek = (GameObject)Resources.Load("シュレック");
-
-        Instantiate(shrek, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(pink, new Vector2(-5, 0), Quaternion.identity);
+        Instantiate(purple1, new Vector2(5, 0), Quaternion.identity);
     }
     public void Lip()
     {
         GameObject black3 = (GameObject)Resources.Load("黒3");
         GameObject purple2 = (GameObject)Resources.Load("紫2");
 
-        Instantiate(black3, new Vector2(-3, 0), Quaternion.identity);
-        Instantiate(purple2, new Vector2(3, 0), Quaternion.identity);
+        Instantiate(black3, new Vector2(-5, 0), Quaternion.identity);
+        Instantiate(purple2, new Vector2(5, 0), Quaternion.identity);
     }
     public void Purple1()
     {
         GameObject eyebrows = (GameObject)Resources.Load("まゆ毛");
 
-        Instantiate(eyebrows, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(eyebrows, new Vector2(5, 0), Quaternion.identity);
     }
     public void Purple2()
     {
         GameObject frieza = (GameObject)Resources.Load("フリーザ");
 
-        Instantiate(frieza, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(frieza, new Vector2(5, 0), Quaternion.identity);
     }
     public void Pink()
     {
         GameObject falseeyelashes = (GameObject)Resources.Load("つけま");
 
-        Instantiate(falseeyelashes, new Vector2(0, 0), Quaternion.identity);
+        Instantiate(falseeyelashes, new Vector2(5, 0), Quaternion.identity);
     }
     public void Falseeyelashes()
     {
         GameObject thinfalseeyelashes = (GameObject)Resources.Load("細今どきつけま");
         GameObject bassavasa = (GameObject)Resources.Load("バッサバサ");
 
-        Instantiate(thinfalseeyelashes, new Vector2(-3, 0), Quaternion.identity);
-        Instantiate(bassavasa, new Vector2(3, 0), Quaternion.identity);
+        Instantiate(thinfalseeyelashes, new Vector2(-5, 0), Quaternion.identity);
+        Instantiate(bassavasa, new Vector2(5, 0), Quaternion.identity);
     }
     public void Bassavasa()
     {
@@ -233,7 +228,23 @@ public class Branch : MonoBehaviour
         GameObject thick = (GameObject)Resources.Load("太");
         GameObject thin = (GameObject)Resources.Load("細");
 
-        Instantiate(thick, new Vector2(-3, 0), Quaternion.identity);
-        Instantiate(thin, new Vector2(3, 0), Quaternion.identity);
+        Instantiate(thick, new Vector2(-5, 0), Quaternion.identity);
+        Instantiate(thin, new Vector2(5, 0), Quaternion.identity);
+    }
+    public void Event()
+    {
+        GameObject _event = (GameObject)Resources.Load("イベント発生");
+
+        Instantiate(_event, new Vector2(15, 3), Quaternion.identity);
+    }
+    public void EventLoad()
+    {
+        _event = PlayerPrefs.GetInt("EVENT", 0);
+
+        if (_event == 1)
+            SceneManager.LoadScene("EyeShadow");
+
+        if (_event == 2)
+            SceneManager.LoadScene("EyebrowEvent");
     }
 }
--- kesyou/Assets/C#/Result.cs
+++ kesyou/Assets/C#/Result.cs
@@ -19,17 +19,27 @@ public class Result : MonoBehaviour
         }
         if (result == 4)
         {
-            GameObject ordinarygirl = (GameObject)Resources.Load("アムラー");
-            GameObject ordinarybackground = (GameObject)Resources.Load("アムラー　背景　ぼかし");
+            GameObject amra = (GameObject)Resources.Load("アムラー");
+            GameObject amrabackground = (GameObject)Resources.Load("アムラー　背景　ぼかし");
 
-            Instantiate(ordinarygirl, new Vector2(-3, -0.67f), Quaternion.identity);
-            Instantiate(ordinarybackground, new Vector3(-3, -0.5f, 1), Quaternion.identity);
+            Instantiate(amra, new Vector2(-3, -0.67f), Quaternion.identity);
+            Instantiate(amrabackground, new Vector3(-3, -0.5f, 1), Quaternion.identity);
         }
-    }
+        if(result == 9)
+        {
+            GameObject shrek = (GameObject)Resources.Load("シュレック");
+            GameObject shrekbackground = (GameObject)Resources.Load("シュレック　背景　ぼかし");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Instantiate(shrek, new Vector2(-3, -0.66f), Quaternion.identity);
+            Instantiate(shrekbackground, new Vector3(-3, -0.5f, 1), Quaternion.identity);
+        }
+        if(result == 10)
+        {
+            GameObject piccolo = (GameObject)Resources.Load("ピッコロ");
+            GameObject piccolobackground = (GameObject)Resources.Load("ピッコロ　背景　ぼかし");
+
+            Instantiate(piccolo, new Vector2(-3, -0.66f), Quaternion.identity);
+            Instantiate(piccolobackground, new Vector3(-3, -0.5f, 1), Quaternion.identity);
+        }
     }
 }
